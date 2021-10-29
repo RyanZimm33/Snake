b1ebbdeed9c3572542f333e4f4467cad9da0b351
Carson Jones <djcubed12@gmail.com>
2021-10-29 18:00:44 -0500

    Create main file

---
+++ snake.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+# While loop section
+def main():
+    """Start game."""
+    pass
+
+# Snake class section
+
+
+# Random generation section
+
+
+if __name__ == '__main__':
+    pygame.init()
+
+    main()
