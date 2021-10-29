f193591af65ab032b734771db82bb894fe4c87d7
Carson Jones <djcubed12@gmail.com>
2021-10-29 18:16:16 -0500

    Base code

--- snake.py
+++ snake.py
@@ -4,7 +4,17 @@ import pygame
 # While loop section
 def main():
     """Start game."""
-    pass
+    global pygame
+
+    screen = pygame.display.set_mode((800, 600))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.update()
+
 
 # Snake class section
 
