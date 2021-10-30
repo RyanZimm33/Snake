82003c65f30cba868e358f2f2312cb4b29e23996
Carson Jones <djcubed12@gmail.com>
2021-10-30 00:29:27 -0500

    Cleaning up

--- snake.py
+++ snake.py
@@ -1,18 +1,13 @@
-import pygame, random
+import random
+import pygame
 
 
 screen_width = 800
 screen_height = 600
-
-cell_height_count = 12
-cell_width_count = 16
 cell_size = 50
-clock = pygame.time.Clock()
 
-cell_size = 50
 blocks_x = screen_width // cell_size
 blocks_y = screen_height // cell_size
-
 
 
 def main():
@@ -24,7 +19,7 @@ def main():
 
     map = [[0 for x in range(blocks_x)] for y in range(blocks_y)]
 
-    # Up, down, left, right
+    # Control keys in order of up, down, left, right
     p1controls = [pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d]
     p2controls = [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]
 
@@ -35,34 +30,12 @@ def main():
     Fruit(screen, map)
 
     while handle_events(players):
-        # DEBUG
         clock.tick(5)
         for p in players:
             p.move(screen)
             p.draw(screen)
 
-        # fruit.drawFruit(screen)
-        # if(fruit.collision(p1.getX(), p1.getY())):
-        #     p1.grow()
-        # if(fruit.collision(p2.getX(), p2.getY())):
-        #     p2.grow()
         pygame.display.update()
-
-def game_intro(screen):
-    intro =True
-    while (intro):
-        myfont = pygame.font.SysFont("Britannic Bold", 40)
-        myfont2 = pygame.font.SysFont("Britannic Bold", 30)
-        title = myfont.render("Snake, but actually Tron", 1, (255, 0, 0))
-        under = myfont2.render("Press Space to Begin", 1, (255, 0, 0))
-        screen.blit(title,(30, 30))
-        screen.blit(under,(30, 80))
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    intro = False
-                    screen.fill((0,0,0))
 
 def handle_events(players):
     """Iterate through events and send them to their proper handlers.
@@ -86,33 +59,46 @@ def handle_events(players):
 
     return True
 
+def game_intro(screen):
+    """Starting screen. Displays the game's name, controls, and anything else needed."""
+    myfont = pygame.font.SysFont("Britannic Bold", 40)
+    myfont2 = pygame.font.SysFont("Britannic Bold", 30)
+
+    title = myfont.render("Snake, but actually Tron", 1, (255, 0, 0))
+    under = myfont2.render("Press Space to Begin", 1, (255, 0, 0))
+
+    screen.blit(title, (30, 30))
+    screen.blit(under, (30, 80))
+    pygame.display.flip()
+
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    intro = False
+                    screen.fill((0, 0, 0))
+
+
 class Fruit:
+    """Object to be eaten by snakes. Allows snakes to grow."""
+
     def __init__(self, screen, map):
+        """Randomly place the fruit and update map to show it."""
         self.x = random.randint(0, blocks_x - 1)
         self.y = random.randint(0, blocks_y - 1)
         map[self.y][self.x] = 2
-        self.drawFruit(screen)
 
-    def drawFruit(self, screen):
+        # Render the fruit
         x, y = index_to_pixels(self.x, self.y)
         fruit = pygame.Rect(x, y, cell_size, cell_size)
         pygame.draw.rect(screen, (255, 0, 0), fruit)
 
-    # def collision(self, xPos, yPos):
-    #     if self.x * cell_size == int(xPos) and self.y * cell_size == int(yPos):
-    #         self.x = random.randint(0, cell_width_count - 1)
-    #         self.y = random.randint(0, cell_height_count - 1)
-    #         return True
-    #     else:
-    #         return False
-
-
-
 class Snake:
-    """A snake with its own length and position."""
+    """A snake. Moves forward every cycle and contains event handlers for turning."""
 
     def __init__(self, X, Y, map, *, controls=None, color=(255, 255, 255)):
-        """Create snake.
+        """Create a snake at x, y. Update the map to include it's position. Use controls and color if given.
 
         Parameters
         ----------
@@ -125,6 +111,7 @@ class Snake:
         color : tuple
             The rgb color of the snake. Default is white for debugging.
         """
+        # handle_events will call the method that matches the event (control) key.
         try:
             self.ch_dir = {
                 controls[0]: self.up,
@@ -134,6 +121,7 @@ class Snake:
                 pygame.K_g: self.grow    # Debug
             }
         except TypeError:
+            # No controls were given.
             self.ch_dir = dict()
 
         self.X = X
@@ -151,52 +139,49 @@ class Snake:
         self.growing = False
 
 
-    def grow(self):
-        self.growing = True
-
-
     def move(self, screen):
-        """Move one position."""
-        # The new coord modulo the max coord. This means the snake will wrap back to zero.
+        """Move forward one cell. Check the map for collisions with snakes or fruits."""
+        # The new coord modulo the max coord. This means the snake will wrap back to the beginning.
         new_X = (self.X + self.dX) % blocks_x
         new_Y = (self.Y + self.dY) % blocks_y
 
         obstacle = self.map[new_Y][new_X]
         if obstacle == 1:
-            print('Snake Collision')
+            # Snake Collision
             return None
         elif obstacle == 2:
-            print('Apple Collision')
+            # Apple Collision
             Fruit(screen, self.map)
             self.growing = True
 
         self.X = new_X
         self.Y = new_Y
         self.body.append((self.X, self.Y))
-
         self.map[self.Y][self.X] = 1
 
-        if not self.growing:
+        if self.growing:
+            # Do NOT remove the tail
+            self.growing = False
+        else:
+            # DO remove the tail
             self.tail = self.body.pop(0)
             self.map[self.tail[1]][self.tail[0]] = 0
-        else:
-            self.growing = False
 
     def draw(self, screen):
+        """Render the snake by drawing a new body cell, and covering the last body cell."""
         head = self.body[-1]
         tail = self.tail
 
         black = (0, 0, 0)
 
-        # erase tail
+        # Erase tail
         tail_coord = index_to_pixels(tail[0], tail[1])
         pygame.draw.rect(screen, black, (tail_coord[0], tail_coord[1], cell_size, cell_size))
 
-        # draw new head
+        # Draw new head
         head_coord = index_to_pixels(head[0], head[1])
         pygame.draw.rect(screen, self.color, (head_coord[0], head_coord[1], cell_size, cell_size))
 
-        # render
         pygame.display.update()
 
 
@@ -220,21 +205,12 @@ class Snake:
         if not (self.dX == -1):
             self.dX, self.dY = 1, 0
 
-    def getX(self):
-        return self.X
-
-    def getY(self):
-        return self.Y
-
 
 def index_to_pixels(x, y):
     """Convert the index coordinates to pixel coordinates. Returns (x, y) in pixels."""
     new_x = x * cell_size
     new_y = y * cell_size
     return new_x, new_y
-
-
-# Random generation section
 
 
 if __name__ == '__main__':
